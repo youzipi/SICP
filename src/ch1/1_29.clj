b4cb6d0da670dc58fd75146e03bcde4c2ee9096a
youzipi <blake_12@qq.com>
2017-01-17 10:13:18 +0800

    fix

--- src/ch1/1_29.clj
+++ src/ch1/1_29.clj
@@ -3,6 +3,7 @@
 
 
 
+
 (defn fun
   "docstring"
   [f, a, b, n]
@@ -11,13 +12,17 @@
   ; x_k = a + k * h
   ;
   (let [h (/ (- b a) n)]
-    (letfn [(x_k [k]
+    (letfn [
+            ;x_k
+            (x_k [k]
               (+ a
                  (* k h))
               ),
+            ;y_k
             (y_k [k]
               (f (x_k k))
               ),
+
             (ret [k]
               (cond
                 (= k 0) (y_k k)
@@ -25,11 +30,11 @@
                           (y_k k)
                           (ret (- k 1))
                           )
-                (= 0 (rem n 2)) (+
+                (= 0 (rem k 2)) (+
                                   (* 2 (y_k k))
                                   (ret (- k 1))
                                   )
-                (= 1 (rem n 2)) (+
+                (= 1 (rem k 2)) (+
                                   (* 4 (y_k k))
                                   (ret (- k 1))
                                   ))
@@ -42,9 +47,13 @@
 
 
 
-
+;
 (println (fun (fn [x] (* x x)), 1.0, 2.0, 1000))
 (println (fun (fn [x] (* x x)), 0, 1.0, 1000))
 (println (fun (fn [x] (* 2 x)), 1.0, 2.0, 1000))
 (println (fun (fn [x] (* 2 x)), 0, 1.0, 1000))
+(println (fun (fn [x] (* x x -1)), -1000.0, 1000.0, 1000))
+
+(println (fun (fn [x] (* x x x)), 0, 1, 8))
+(println (fun (fn [x] (* x x x)), 0, 1, 2))
 
---
+++ src/ch1/common.clj
@@ -0,0 +1,7 @@
+(ns ch1.common)
+
+
+(defn abs [x]
+  (if (> x 0) x
+              (- 0 x))
+  )
